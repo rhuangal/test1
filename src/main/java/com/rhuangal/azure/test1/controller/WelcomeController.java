b5aa8e31dc9e4d32ef8db2fcbba3f6b23cf388be
rhuangal <kirasoft@gmail.com>
2023-02-09 10:49:09 -0500

    initial

---
+++ src/main/java/com/rhuangal/azure/test1/controller/WelcomeController.java
@@ -0,0 +1,36 @@
+package com.rhuangal.azure.test1.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WelcomeController {
+    
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "welcome"; //view
+    }
+
+    // /hello?name=kotlin
+    @GetMapping("/hello")
+    public String mainWithParam(@RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
+    }
+}
